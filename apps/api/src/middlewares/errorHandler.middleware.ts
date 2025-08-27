494a95349b992444e34ba992aab6364cc9fa8c4f
harish kumar <harishkumarharishkumar108@gmail.com>
2025-08-27 21:14:30 +0530

    Fix: added global utility to manage response

--- apps/api/src/middlewares/errorHandler.middleware.ts
+++ apps/api/src/middlewares/errorHandler.middleware.ts
@@ -1,5 +1,8 @@
+import { ApiResponse } from "@repo/shared/types";
 import { ErrorCode } from "@repo/shared/errors";
+
 import logger from "../utils/logger.js";
+import { sendResponse } from "../utils/response.js";
 
 import { ErrorRequestHandler } from "express";
 
@@ -9,18 +12,21 @@ export const errorHandler: ErrorRequestHandler = (err, req, res, _next) => {
   if (err instanceof CustomError) {
     logger.error(err.message, { errorCode: err.errorCode });
 
-    return res.status(err.statusCode).json({
+    const payload: ApiResponse = {
       success: false,
       code: err.errorCode,
       message: err.message,
       ...(err instanceof InvalidArgument ? { errors: err.errors } : {}),
-    });
+    };
+    sendResponse(res, err.statusCode, payload);
   }
 
   logger.error("Unexpected error", err);
-  res.status(500).json({
+
+  const payload: ApiResponse = {
     success: false,
     code: ErrorCode.INTERNAL_ERROR,
     message: "Unexpected server error",
-  });
+  };
+  sendResponse(res, 500, payload);
 };
--- packages/shared/src/api-response.ts
+++ packages/shared/src/api-response.ts
@@ -1,6 +1,7 @@
 export type ApiResponse<T = any> = {
   success: boolean;
   message?: string;
+  code?: string;
   data?: T;
   errors?: string[];
 };
