c88a2609391f23b50a1c4610ca5cf034a9ed6c0d
harish kumar <harishkumarharishkumar108@gmail.com>
2026-01-29 11:13:15 +0530

    config(frontend): added test data file

---
+++ apps/web/lib/test_data/index.tsx
@@ -0,0 +1,18 @@
+export const test_steps: Step[] = [
+  {
+    label: "Step 1",
+    Icon: () => <>Step 1</>,
+  },
+  {
+    label: "Step 2",
+    Icon: () => <>Step 2</>,
+  },
+  {
+    label: "Step 3",
+    Icon: () => <>Step 3</>,
+  },
+  {
+    label: "Step 4",
+    Icon: () => <>Step 4</>,
+  },
+];
