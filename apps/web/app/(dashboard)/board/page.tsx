35457afdf4f92c8f977bfc2e47e86cb23778f531
harish kumar <harishkumarharishkumar108@gmail.com>
2026-08-15 16:19:22 +0530

    feat(layout): adjust main padding for improved layout consistency in AppLayout feat(board): update height calculation for better responsiveness in BoardPage

--- apps/web/app/(dashboard)/board/page.tsx
+++ apps/web/app/(dashboard)/board/page.tsx
@@ -21,7 +21,7 @@ export default function BoardPage() {
 
   return (
     <DragDropProvider>
-      <div className="h-[calc(100vh-124px)] flex-1 flex flex-col gap-8">
+      <div className="h-[calc(100vh-92px)] md:h-[calc(100vh-124px)] flex-1 flex flex-col gap-8">
         <HeadSection />
         <Board />
       </div>
--- apps/web/layouts/AppLayout.tsx
+++ apps/web/layouts/AppLayout.tsx
@@ -6,7 +6,6 @@ import Header from '@/components/shared/Header/Header';
 import NavBar from '@/components/shared/NavBar/NavBar';
 import useBreakPoints from '@/lib/hooks/useBreakpoints';
 
-
 interface AppLayoutProps extends PropsWithChildren {}
 
 const AppLayout: React.FC<AppLayoutProps> = (props) => {
@@ -24,7 +23,7 @@ const AppLayout: React.FC<AppLayoutProps> = (props) => {
       <NavBar isNavOpen={isNavOpen} closeNav={() => setIsNavOpen(false)} />
       <div className="flex-1">
         <Header isNavOpen={isNavOpen} toggleNav={() => setIsNavOpen((prev) => !prev)} />
-        <main className="flex min-h-[calc(100vh-64px)] p-8">{children}</main>
+        <main className="flex min-h-[calc(100vh-64px)] py-4 px-2 md:p-8">{children}</main>
       </div>
     </div>
   );
